89a0e586ad0f2b86648dcd6d8d6de85cb09cef74
dmitri <dmitri.mahayana@gmail.com>
2026-02-20 23:09:45 +0700

    fix: update icons

--- src/components/common/tool.vue
+++ src/components/common/tool.vue
@@ -10,14 +10,17 @@ interface Props {
 	tool: Tool;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const isImage = () => props.tool.icon.endsWith('.svg') || props.tool.icon.endsWith('.png') || props.tool.icon.endsWith('.jpg');
 </script>
 
 <template>
 	<div class="column card is-narrow p-0 is-clickable tool">
 		<div class="column is-narrow is-flex is-align-items-center py-0">
 			<div class="column is-3 is-narrow-mobile icon">
-				<i :class="tool.icon" :style="{ color: tool.color }"></i>
+				<i v-if="!isImage()" :class="tool.icon" :style="{ color: tool.color }"></i>
+				<img v-else :src="tool.icon" :alt="tool.name" />
 			</div>
 			<div class="column has-text-left is-narrow">
 				<div class="name">{{ tool.name }}</div>
@@ -30,18 +33,33 @@ defineProps<Props>();
 <style lang="scss" scoped>
 .tool {
 	background-color: var(--card-bg-color);
+	// .icon {
+	// 	position: relative;
+	// 	padding: 30px;
+	// 	font-size: 40px;
+	// 	border-radius: 8px;
+	// 	background-color: var(--text-color);
+
+	// 	i {
+	// 		top: 50%;
+	// 		right: 50%;
+	// 		position: absolute;
+	// 		transform: translate(50%, -50%);
+	// 	}
+	// }
 	.icon {
 		position: relative;
 		padding: 30px;
 		font-size: 40px;
 		border-radius: 8px;
 		background-color: var(--text-color);
+		display: flex;
+		align-items: center;
+		justify-content: center;
 
-		i {
-			top: 50%;
-			right: 50%;
-			position: absolute;
-			transform: translate(50%, -50%);
+		i, img {
+			width: auto;
+			height: auto;
 		}
 	}
 	.name {
